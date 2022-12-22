03a2f1e9d267d8f262a16648eafcbf39e9e23d0b
Vyacheslav Agafonov <>
2022-12-22 23:38:25 +0300

    Fix linter errors

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
+
 	"golang.org/x/example/stringutil"
 )
 
 func main() {
-	var text string = "Hello, OTUS!"
+	text := "Hello, OTUS!"
 	fmt.Println(stringutil.Reverse(text))
 }
