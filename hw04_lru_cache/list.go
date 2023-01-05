446889c36d3a966c6ad2565a0430b728675df838
Vyacheslav Agafonov <>
2023-01-05 22:31:29 +0300

    HW4 draft double linked list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,61 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head   ListItem
+	tail   ListItem
+	length int
+}
+
+func (l list) Len() int {
+	return l.length
+}
+
+// Front Первый элемент списка
+func (l list) Front() *ListItem {
+	return &l.head
+}
+
+// Back Последний элемент списка
+func (l list) Back() *ListItem {
+	return nil
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := ListItem{v, nil, nil}
+
+	if l.head == (ListItem{}) { // Проверка на пустое значение свойства head. Это ОК ?
+		l.tail = item
+	} else {
+		item.Next = &l.head
+		l.head.Prev = &item
+	}
+
+	l.head = item
+	l.length++
+	return &item
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := ListItem{v, nil, nil}
+
+	if l.tail == (ListItem{}) { // Проверка на пустое значение свойства head. Это ОК ?
+		l.head = item
+	} else {
+		l.tail.Next = &item
+		item.Prev = &l.tail
+	}
+
+	l.tail = item // после этого присваивания item.Prev измененятеся с последенего значения в списке (10) на себя самого (20, 30)
+	l.length++
+	return &item
+}
+
+func (l list) Remove(i *ListItem) {
+
+}
+
+func (l list) MoveToFront(i *ListItem) {
+
 }
 
 func NewList() List {
