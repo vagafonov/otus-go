7c8a59180035991617f8cc424ef048de102b932e
Vyacheslav Agafonov <>
2023-01-06 15:38:34 +0300

    HW4 double linked list done

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -2,6 +2,7 @@ package hw04lrucache
 
 type List interface {
 	Len() int
+	Dump() []interface{} //TODO ? Почему нельзя указать метод для структуры которого нет в интерфейсе. Возникает ошибка l.Dump undefined (type List has no field or method Dump) ?
 	Front() *ListItem
 	Back() *ListItem
 	PushFront(v interface{}) *ListItem
@@ -17,8 +18,8 @@ type ListItem struct {
 }
 
 type list struct {
-	head   ListItem
-	tail   ListItem
+	head   *ListItem
+	tail   *ListItem
 	length int
 }
 
@@ -28,50 +29,91 @@ func (l list) Len() int {
 
 // Front Первый элемент списка
 func (l list) Front() *ListItem {
-	return &l.head
+	return l.head
 }
 
 // Back Последний элемент списка
 func (l list) Back() *ListItem {
-	return nil
+	return l.tail
 }
 
 func (l *list) PushFront(v interface{}) *ListItem {
 	item := ListItem{v, nil, nil}
 
-	if l.head == (ListItem{}) { // Проверка на пустое значение свойства head. Это ОК ?
-		l.tail = item
+	if l.head == nil {
+		l.head = &item
+		l.tail = &item
 	} else {
-		item.Next = &l.head
+		item.Next = l.head
 		l.head.Prev = &item
 	}
 
-	l.head = item
+	l.head = &item
 	l.length++
 	return &item
 }
 
 func (l *list) PushBack(v interface{}) *ListItem {
+	/*
+		TODO ?
+		Является ли объявление структуры с "&" в начале &ListItem{v, nil, nil}
+		и без дальнейшего использования взятия указателя "&" от переменной
+		тем же самым что реализовано ниже. Если да то какой вариант предпочтительнее ?
+	*/
+
 	item := ListItem{v, nil, nil}
 
-	if l.tail == (ListItem{}) { // Проверка на пустое значение свойства head. Это ОК ?
-		l.head = item
+	if l.tail == nil { // Проверка на пустое значение свойства head. Это ОК ?
+		l.head = &item
 	} else {
+		item.Prev = l.tail
 		l.tail.Next = &item
-		item.Prev = &l.tail
 	}
 
-	l.tail = item // после этого присваивания item.Prev изменяется с последнего значения в списке (10) на себя самого (20, 30) а так же
+	l.tail = &item
 	l.length++
 	return &item
 }
 
-func (l list) Remove(i *ListItem) {
+func (l *list) Remove(i *ListItem) {
 
+	// Удаление не первого и не последнего элемента
+	if i.Prev != nil && i.Next != nil {
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+	} else if i.Prev == nil { // Удаление первого элемента
+		i.Next.Prev = nil
+		// TODO ? Мало удалить сам элемент (i = nil) кроме этого нужно удалить все сылки на него l.head, i.Next.Prev ?
+		l.head = i.Next
+	} else if i.Next == nil { // Удаление последнего элемента
+		i.Prev.Next = nil
+		// TODO ? Мало удалить сам элемент (i = nil) кроме этого нужно удалить все сылки на него l.head, i.Next.Prev ?
+		l.tail = i.Prev
+	}
+
+	i = nil
+	l.length--
 }
 
-func (l list) MoveToFront(i *ListItem) {
+func (l *list) Dump() []interface{} {
+	i := l.Front()
+	res := make([]interface{}, 0)
+	for i != nil {
+		res = append(res, i.Value)
+		i = i.Next
+	}
+	return res
+}
 
+func (l *list) MoveToFront(i *ListItem) {
+	// Перемещение последнего в конец
+	if i.Next == nil {
+		i.Prev.Next = nil
+		i.Prev = nil
+		i.Next = l.head
+		l.head = i
+	}
+	// Перемещение первого в начало бессмысленно
 }
 
 func NewList() List {
--- hw04_lru_cache/list_test.go
+++ hw04_lru_cache/list_test.go
@@ -48,4 +48,104 @@ func TestList(t *testing.T) {
 		}
 		require.Equal(t, []int{70, 80, 60, 40, 10, 30, 50}, elems)
 	})
+
+	t.Run("remove first", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.PushBack(20)  // [10, 20]
+		l.PushBack(30)  // [10, 20, 30]
+
+		first := l.Front() // 10
+		l.Remove(first)    // [20, 30]
+		require.Equal(t, 2, l.Len())
+
+		require.Equal(t, 20, l.Front().Value)
+		require.Equal(t, 30, l.Back().Value)
+	})
+
+	t.Run("remove last", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.PushBack(20)  // [10, 20]
+		l.PushBack(30)  // [10, 20, 30]
+
+		last := l.Back() // 30
+		l.Remove(last)   // [10, 20]
+
+		require.Equal(t, 2, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 20, l.Back().Value)
+	})
+
+	t.Run("remove last of two", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.PushBack(20)  // [10, 20]
+
+		last := l.Back() // 20
+		l.Remove(last)   // [10]
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+	})
+
+	t.Run("remove first of two", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+		l.PushBack(20)  // [10, 20]
+
+		last := l.Front() // 10
+		l.Remove(last)    // [20]
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 20, l.Back().Value)
+	})
+
+	t.Run("only pushes front", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(30) // [10]
+		l.PushFront(20) // [10]
+		l.PushFront(10) // [10]
+
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 20, l.Front().Next.Value)
+		require.Equal(t, 30, l.Front().Next.Next.Value)
+	})
+
+	t.Run("only pushes back", func(t *testing.T) {
+		l := NewList()
+
+		l.PushBack(10) // [10]
+		l.PushBack(20) // [10]
+		l.PushBack(30) // [10]
+
+		require.Equal(t, 3, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 20, l.Front().Next.Value)
+		require.Equal(t, 30, l.Front().Next.Next.Value)
+	})
+
+	t.Run("pushe one", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+
+		require.Equal(t, 1, l.Len())
+		require.Equal(t, 10, l.Front().Value)
+		require.Equal(t, 10, l.Back().Value)
+	})
+
+	t.Run("dump", func(t *testing.T) {
+		l := NewList()
+
+		l.PushFront(10) // [10]
+
+		require.Equal(t, append(make([]interface{}, 0), 10), l.Dump())
+	})
 }
