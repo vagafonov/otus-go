570d324e925e469491b9d304ce69726fe6372538
Vyacheslav Agafonov <v.agafonov@webinar.ru>
2023-03-13 20:50:55 +0300

    HW08 done

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	environ := os.Environ()
+	_ = environ
+
+	for key, value := range env {
+		os.Setenv(key, value.Value)
+	}
+
+	result, err := exec.Command(cmd[0], cmd[1:]...).Output() //nolint:gosec
+	if err != nil {
+		return 0
+	}
+
+	fmt.Println(string(result))
+
+	return 1
 }
